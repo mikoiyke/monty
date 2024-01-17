be2045929c2b53be1827a80ff21b942cc01c34ac
Eveshogweyore <alleeveshogweyore@gmail.com>
2024-01-17 03:51:02 -0800

    Update monty.h file.

--- monty.h
+++ monty.h
@@ -24,6 +24,12 @@ typedef struct stack_s
 	struct stack_s *next;
 } stack_t;
 
+void push(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+
 /**
  * struct instruction_s - opcode and its function
  * @opcode: the opcode
@@ -37,11 +43,5 @@ typedef struct instruction_s
 	char *opcode;
 	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
-
-void push(stack_t **stack, unsigned int line_number);
-void pop(stack_t **stack, unsigned int line_number);
-void pall(stack_t **stack, unsigned int line_number);
-void pint(stack_t **stack, unsigned int line_number);
-void nop(stack_t **stack, unsigned int line_number);
 
 #endif /* MONTY_H */
