68edf48d6cb851322cfafb6cc9b6d8d82f572621
Eveshogweyore <alleeveshogweyore@gmail.com>
2024-01-17 11:46:23 -0800

    Function to free stack when done.

---
+++ free_stack.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+/**
+ * free_stack - free up the memory used up by the stack
+ * @stack: Pointer to first element in stack.
+ *
+ * Return: Nothing.
+ */
+void free_stack(stack_t *stack)
+{
+	stack_t *prev = stack; 
+
+	while (prev)
+	{
+		stack = stack->next;
+		free(prev);
+		prev = stack;
+	}
+}
