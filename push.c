4501ed9b226a48ec9ff1ae3d6dd490dd3254a9f5
Eveshogweyore <alleeveshogweyore@gmail.com>
2024-01-17 07:57:17 -0800

    Adds new element(s) to the stack.

--- push.c
+++ push.c
@@ -1,6 +1,29 @@
 #include "monty.h"
 
+/**
+ * push - pushes an element to the stack
+ * @stack: Pointer to the top of stack.
+ * @line_number:  Current line number for the monty bytecode.
+ *
+ * Return: Nothing.
+ */
 void push(stack_t **stack, unsigned int line_number)
 {
-	return;
+	stack_t *new_node, *curr = *stack;
+
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	new_node->n = data;
+
+	new_node->prev = NULL;
+	new_node->next = curr;
+	if (curr)
+		curr->prev = new_node;
+
+	*stack = new_node;
 }
