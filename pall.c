46ce40d478e635358892361db2182abf3576fa70
Eveshogweyore <alleeveshogweyore@gmail.com>
2024-01-17 07:56:41 -0800

    Print all elements in the stack.

--- pall.c
+++ pall.c
@@ -1,6 +1,19 @@
 #include "monty.h"
 
+/**
+ * pall - prints all elements in the stack
+ * @stack: Pointer to the top of stack.
+ * @line_number:  Current line number for the monty bytecode.
+ *
+ * Return: Nothing.
+ */
 void pall(stack_t **stack, unsigned int line_number)
 {
-	return;
+	stack_t *curr = *stack;
+
+	while (curr)
+	{
+		printf("%d\n", curr->n);
+		curr = curr->next;
+	}
 }
