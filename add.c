46c39daf960dff6d19245dd2319a4a7fdcd77d97
mikoiyke <mikoiyke@gmail.com>
2024-01-18 08:12:23 -0800

    add.c file created

---
+++ add.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * add - prints all the values on the stack,
+ * starting from the top of the stack.
+ * @stack: Pointer to the top of stack.
+ * @line_number:  Current line number for the monty bytecode.
+ *
+ * Return: Nothing.
+ */
+void add(stack_t **stack, unsigned int line_number)
+{
+}
