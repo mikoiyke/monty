e65b9173f6f2c42766e60af9ff8d39d50c1cd337
mikoiyke <mikoiyke@gmail.com>
2024-01-18 08:13:56 -0800

    added prototype for add opcode

--- monty.h
+++ monty.h
@@ -30,6 +30,7 @@ void pall(stack_t **stack, unsigned int line_number);
 void pint(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
+void add(stack_t **stack, unsigned int line_number);
 
 FILE *open_file(char *file, char *mode, int argc);
 void free_stack(stack_t *stack);
