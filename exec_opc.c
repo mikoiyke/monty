954551bcf6db8fd76453ded031bedf608180182b
Eveshogweyore <alleeveshogweyore@gmail.com>
2024-01-18 02:08:08 -0800

    COnform function to Betty standard

--- exec_opc.c
+++ exec_opc.c
@@ -1,7 +1,12 @@
 #include "monty.h"
 
 /**
+ * exec_opc - executes the opcodes
+ * @stack: Pointer to stack.
+ * @opc: The current opcode to be executed.
+ * @l: The current line number.
  *
+ * Return: Nothing.
  */
 void exec_opc(stack_t **stack, char *opc, unsigned int l)
 {
