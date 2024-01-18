4b7e5341a25f22fd0b00feea417e447a64897c9f
Eveshogweyore <alleeveshogweyore@gmail.com>
2024-01-17 21:24:09 -0800

    Logic to handle empty line instruction added.

--- main.c
+++ main.c
@@ -38,9 +38,10 @@ int main(int argc, char *argv[])
 		if (monty_cmd[1])
 			data = opcode_value(monty_cmd[1]);
 
-		for (i = 0; i < length; i++)
-			if (strcmp(op_fun[i].opcode, monty_cmd[0]) == 0)
-				op_fun[i].f(&stack, ln);
+		if (monty_cmd[0])
+			for (i = 0; i < length; i++)
+				if (strcmp(op_fun[i].opcode, monty_cmd[0]) == 0)
+					op_fun[i].f(&stack, ln);
 		ln++;
 	}
 
