bd937999b749cbceaf3f3745529f46203b4aec5b
Eveshogweyore <alleeveshogweyore@gmail.com>
2024-01-18 07:59:08 -0800

    Logic to swap the values in two nodes.

--- monty.h
+++ monty.h
@@ -31,10 +31,8 @@ void pint(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 
-
 FILE *open_file(char *file, char *mode, int argc);
 void free_stack(stack_t *stack);
-/* int opcode_value(char *opcode, char *value); */
 void exec_opc(stack_t **stack, char *opc, unsigned int l);
 int is_digit(char *c);
 
--- swap.c
+++ swap.c
@@ -1,45 +1,24 @@
 #include "monty.h"
 
-char *data = NULL;
-
 /**
- * push - swaps an element to the stack
+ * swap - swaps an element to the stack
  * @stack: Pointer to the top of stack.
  * @line_number:  Current line number for the monty bytecode.
  *
  * Return: Nothing.
  */
-void push(stack_t **stack, unsigned int line_number)
+void swap(stack_t **stack, unsigned int line_number)
 {
-	stack_t *new_node, *curr = *stack;
+	stack_t *curr = *stack;
+	int temp;
 
-	if (is_digit(data) == 0)
+	if (!curr || !(curr->next))
 	{
-		fprintf(stderr, "L%u: usage: push integer\n", line_number);
-
-		if (*stack)
-			free_stack(*stack);
-
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-
-		if (*stack)
-			free_stack(*stack);
-
-		exit(EXIT_FAILURE);
-	}
-
-	new_node->n = atoi(data);
-
-	new_node->prev = NULL;
-	new_node->next = curr;
-	if (curr)
-		curr->prev = new_node;
-
-	*stack = new_node;
+	temp = curr->n;
+	curr->n = curr->next->n;
+	curr->next->n = temp;
 }
