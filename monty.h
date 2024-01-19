6eab602712d3d5a1a03e647b7c5bd0ddad9b079a
mikoiyke <mikoiyke@gmail.com>
2024-01-18 18:46:43 -0800

    Added prototype for sub opcode

--- monty.h
+++ monty.h
@@ -31,6 +31,7 @@ void pint(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **stack, unsigned int line_number);
 void nop(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
 
 FILE *open_file(char *file, char *mode, int argc);
 void free_stack(stack_t *stack);
