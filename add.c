147be1a8009fb35514a0bbe8919d74e4c7826f1c
Eveshogweyore <alleeveshogweyore@gmail.com>
2024-01-19 00:21:57 -0800

    Adjusted the functions doc.

--- add.c
+++ add.c
@@ -1,8 +1,7 @@
 #include "monty.h"
 
 /**
- * add - prints all the values on the stack,
- * starting from the top of the stack.
+ * add - adds the top two elements of the stack.
  * @stack: Pointer to the top of stack.
  * @line_number:  Current line number for the monty bytecode.
  *
