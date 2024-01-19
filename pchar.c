30bed425a76a28a3b82281c2bf61096ce69c9245
Eveshogweyore <alleeveshogweyore@gmail.com>
2024-01-19 01:28:34 -0800

    Added new line character to error messages.

--- pchar.c
+++ pchar.c
@@ -12,13 +12,13 @@ void pchar(stack_t **stack, unsigned int line_number)
 {
 	if (!(*stack))
 	{
-		fprintf(stderr, "L%u: can't pchar, stack empty", line_number);
+		fprintf(stderr, "L%u: can't pchar, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
 	if ((*stack)->n < 0 || (*stack)->n > 127)
 	{
-		fprintf(stderr, "L%u: can't pchar, value out of range", line_number);
+		fprintf(stderr, "L%u: can't pchar, value out of range\n", line_number);
 	}
 	printf("%c\n", (*stack)->n);
 }
